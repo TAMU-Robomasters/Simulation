5eb9f0bf9d76650cdaa7ef7f623811cbdb5fdbc4
lai-huy <98334390+lai-huy@users.noreply.github.com>
2022-02-28 22:13:53 -0600

    Add ability to write to file.
    The file name is:
    SimRun-yyyy-MM-dd-HH-mm-ss.csv

--- Assets/Scripts/GetDepth.cs
+++ Assets/Scripts/GetDepth.cs
@@ -1,25 +1,68 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
 using UnityEngine;
 
 public class GetDepth : MonoBehaviour
 {
+	/**
+	 * String for file type
+	 */
+	protected const string file_type = ".csv";
+
+
+	protected UTF8Encoding utf8 = new UTF8Encoding(true);
+
+	protected string path;
+
+	// CharacterController
 	public CharacterController controller;
+
+	/**
+	 * The magnitude of the depth vector.
+	 * The direction of the vector is the same as the camera.
+	 * The vector goes from the camera to any collidable object.
+	 */
 	private float depth = 0f;
+
+	/**
+	 * Magnitude of the velocity vector.
+	 * See vel
+	 */
 	private float velMag = 0f;
+
+	/**
+	 * The Velocity Vector
+	 */
 	private Vector3 vel;
+
+	private StreamWriter GlobalWriter;
 	
     // Start is called before the first frame update
     void Start()
     {
-        
+		path = Directory.GetCurrentDirectory() + "\\SimRun-" + DateTime.Now.ToString("yyyy-MM-ss-HH-mm-ss") + file_type;
+		Debug.Log(path);
+		using (FileStream fs = File.Create(path))
+        {
+			byte[] info = utf8.GetBytes("Depth,Speed,Velocity (x),Velocity (y),Velocity (z)\n");
+			fs.Write(info, 0, info.Length);
+		}
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(depth + ", " + velMag + ", " + vel);
-    }
+		string data = depth + "," + velMag + "," + vel.x + "," + vel.y + "," + vel.z;
+		Debug.Log(data);
+		using (GlobalWriter = File.AppendText(path))
+        {
+			GlobalWriter.WriteLine(data);
+        }
+	}
 	
 	void FixedUpdate() {
 		Vector3 objectForward = transform.TransformDirection(Vector3.forward);
