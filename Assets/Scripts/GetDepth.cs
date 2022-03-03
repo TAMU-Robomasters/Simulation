255354fca38d826b1551be95507bd9c46e433a60
lai-huy <98334390+lai-huy@users.noreply.github.com>
2022-03-02 20:03:14 -0600

    Update GetDepth.cs
    add position and rotation vectors to the csv

--- Assets/Scripts/GetDepth.cs
+++ Assets/Scripts/GetDepth.cs
@@ -9,14 +9,33 @@ using UnityEngine;
 public class GetDepth : MonoBehaviour
 {
 	/**
+	 * TODO:
+	 *	Remove velocity
+	 *	Retrieve position and rotation.
+	 *	Record each run of the simulation.
+	 *	Add time data.
+	 *	
+	 */
+
+	/**
 	 * String for file type
 	 */
 	protected const string file_type = ".csv";
 
-
+	/**
+	 * UTF8Encoding
+	 */
 	protected UTF8Encoding utf8 = new UTF8Encoding(true);
 
+	/**
+	 * Current directory of this project.
+	 */
 	protected string path;
+
+	/**
+	 * Start time of the current simulation run.
+	 */
+	protected DateTime StartTime;
 
 	// CharacterController
 	public CharacterController controller;
@@ -38,17 +57,31 @@ public class GetDepth : MonoBehaviour
 	 * The Velocity Vector
 	 */
 	private Vector3 vel;
+	
+	/**
+	 * The Position Vector
+	 */
+	private Vector3 pos;
 
+	/**
+	 * The Rotation Vector
+	 */
+	private Vector3 rot;
+
+	/**
+	 * StreamWriter to write to csv file.
+	 */
 	private StreamWriter GlobalWriter;
 	
     // Start is called before the first frame update
     void Start()
     {
-		path = Directory.GetCurrentDirectory() + "\\SimRun-" + DateTime.Now.ToString("yyyy-MM-ss-HH-mm-ss") + file_type;
+		StartTime = DateTime.Now;
+		path = Directory.GetCurrentDirectory() + @"\SimRun-" + StartTime.ToString("yyyy-MM-ss-HH-mm-ss") + file_type;
 		Debug.Log(path);
 		using (FileStream fs = File.Create(path))
         {
-			byte[] info = utf8.GetBytes("Depth,Speed,Velocity (x),Velocity (y),Velocity (z)\n");
+			byte[] info = utf8.GetBytes("Depth,Position (x),Position (y),Position (z),Rotation (x), Rotation (y),Rotation (z),Velocity (x),Velocity (y),Velocity (z)\n");
 			fs.Write(info, 0, info.Length);
 		}
     }
@@ -56,8 +89,8 @@ public class GetDepth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		string data = depth + "," + velMag + "," + vel.x + "," + vel.y + "," + vel.z;
-		Debug.Log(data);
+		string data = depth + "," + pos.x + "," + pos.y + "," + pos.z + "," + rot.x + "," + rot.y + "," + rot.z + "," + vel.x + "," + vel.y + "," + vel.z;
+		// Debug.Log(data);
 		using (GlobalWriter = File.AppendText(path))
         {
 			GlobalWriter.WriteLine(data);
@@ -69,16 +102,12 @@ public class GetDepth : MonoBehaviour
 		Debug.DrawRay(transform.position, objectForward, Color.green);
 		Ray ray = new Ray(transform.position, objectForward);
 		RaycastHit hit;
-		if (Physics.Raycast(ray, out hit))
-		{
-			// Debug.Log(hit.distance);
-			depth = hit.distance;
-		} else {
-			depth = -1f;
-		}
+		depth = Physics.Raycast(ray, out hit) ? hit.distance : -1f;
 		
 		Vector3 horizontalVelocity = controller.velocity;
         horizontalVelocity = new Vector3(controller.velocity.x, 0, controller.velocity.z);
+		pos = transform.position;
+		rot = transform.rotation.eulerAngles;
 		vel = horizontalVelocity;
         velMag = horizontalVelocity.magnitude;
 	}
