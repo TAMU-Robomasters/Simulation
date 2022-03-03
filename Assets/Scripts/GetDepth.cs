e13c43f79f8bcc13093080ad2d77820d6e5b62f2
lai-huy <98334390+lai-huy@users.noreply.github.com>
2022-03-03 13:01:17 -0600

    Update GetDepth.cs
    Add time data and make private attributes to protected.

--- Assets/Scripts/GetDepth.cs
+++ Assets/Scripts/GetDepth.cs
@@ -17,11 +17,14 @@ public class GetDepth : MonoBehaviour
 	 *	
 	 */
 
+	// CharacterController
+	public CharacterController controller;
+	
 	/**
 	 * String for file type
 	 */
 	protected const string file_type = ".csv";
-
+	
 	/**
 	 * UTF8Encoding
 	 */
@@ -37,59 +40,65 @@ public class GetDepth : MonoBehaviour
 	 */
 	protected DateTime StartTime;
 
-	// CharacterController
-	public CharacterController controller;
+	/**
+	 * StreamWriter to write to csv file.
+	 */
+	protected StreamWriter GlobalWriter;
+
+	/**
+	 * Difference between DateTime.Now and StartTime
+	 */
+	protected double time;
 
 	/**
 	 * The magnitude of the depth vector.
 	 * The direction of the vector is the same as the camera.
 	 * The vector goes from the camera to any collidable object.
 	 */
-	private float depth = 0f;
+	protected float depth = 0f;
 
 	/**
 	 * Magnitude of the velocity vector.
 	 * See vel
 	 */
-	private float velMag = 0f;
+	protected float velMag = 0f;
 
 	/**
 	 * The Velocity Vector
 	 */
-	private Vector3 vel;
+	protected Vector3 vel;
 	
 	/**
 	 * The Position Vector
 	 */
-	private Vector3 pos;
+	protected Vector3 pos;
 
 	/**
 	 * The Rotation Vector
 	 */
-	private Vector3 rot;
+	protected Vector3 rot;
 
 	/**
-	 * StreamWriter to write to csv file.
-	 */
-	private StreamWriter GlobalWriter;
-	
-    // Start is called before the first frame update
-    void Start()
+     * Start is called before the first frame update
+     */
+	void Start()
     {
 		StartTime = DateTime.Now;
-		path = Directory.GetCurrentDirectory() + @"\SimRun-" + StartTime.ToString("yyyy-MM-ss-HH-mm-ss") + file_type;
+		path = Directory.GetCurrentDirectory() + @"\SimRun-" + StartTime.ToString("yyyy-MM-dd-HH-mm-ss") + file_type;
 		Debug.Log(path);
 		using (FileStream fs = File.Create(path))
         {
-			byte[] info = utf8.GetBytes("Depth,Position (x),Position (y),Position (z),Rotation (x), Rotation (y),Rotation (z),Velocity (x),Velocity (y),Velocity (z)\n");
+			byte[] info = utf8.GetBytes("Time,Depth,Position (x),Position (y),Position (z),Rotation (x), Rotation (y),Rotation (z),Velocity (x),Velocity (y),Velocity (z)\n");
 			fs.Write(info, 0, info.Length);
 		}
     }
 
-    // Update is called once per frame
-    void Update()
+	/**
+     * This method is called once per Render Frame.
+     */
+	void Update()
     {
-		string data = depth + "," + pos.x + "," + pos.y + "," + pos.z + "," + rot.x + "," + rot.y + "," + rot.z + "," + vel.x + "," + vel.y + "," + vel.z;
+		string data = time + "," + depth + "," + pos.x + "," + pos.y + "," + pos.z + "," + rot.x + "," + rot.y + "," + rot.z + "," + vel.x + "," + vel.y + "," + vel.z;
 		// Debug.Log(data);
 		using (GlobalWriter = File.AppendText(path))
         {
@@ -97,15 +106,19 @@ public class GetDepth : MonoBehaviour
         }
 	}
 	
+	/**
+	 * This method is called once per Physics Frame
+	 */
 	void FixedUpdate() {
 		Vector3 objectForward = transform.TransformDirection(Vector3.forward);
 		Debug.DrawRay(transform.position, objectForward, Color.green);
 		Ray ray = new Ray(transform.position, objectForward);
 		RaycastHit hit;
+
+		Vector3 horizontalVelocity = new Vector3(controller.velocity.x, 0, controller.velocity.z);
+
+		time = (DateTime.Now - StartTime).TotalSeconds;
 		depth = Physics.Raycast(ray, out hit) ? hit.distance : -1f;
-		
-		Vector3 horizontalVelocity = controller.velocity;
-        horizontalVelocity = new Vector3(controller.velocity.x, 0, controller.velocity.z);
 		pos = transform.position;
 		rot = transform.rotation.eulerAngles;
 		vel = horizontalVelocity;
